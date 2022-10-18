bdc497c8dbfcd83cbc81c8ff386d8c33afc15bc2
Armine8 <armisha15@mail.ru>
2022-10-18 11:16:47 +0300

    Operators

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -1,14 +1,12 @@
 package lang.print.gaps.task5;
 
-import java.text.MessageFormat;
-
 public class ThreeStuckVars {
     public static void main(String[] args) {
         int first = 1;
         int second = 10;
         int third = 100;
-        String vars = "({0}{1}{2})";
-        String varsOut = MessageFormat.format(vars, first, second, third);
-        System.out.println(varsOut);
+        System.out.print(first);
+        System.out.print(second);
+        System.out.print(third);
     }
 }
